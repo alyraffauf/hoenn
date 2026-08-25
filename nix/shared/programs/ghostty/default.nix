2b6e77cf8a022ba717f160c925b50f04840f2842
Aly Raffauf <aly@aly.codes>
2026-08-24 23:16:32 -0400

    ghostty: support macos

--- nix/shared/programs/ghostty/default.nix
+++ nix/shared/programs/ghostty/default.nix
@@ -1,11 +1,36 @@
 _: {
-  flake = {
-    nixosModules.aly = {pkgs, ...}: {
-      users.users.aly.packages = [pkgs.ghostty];
+  flake = {pkgs, ...}: {
+    darwinModules.default = {
+      environment.systemPackages = [pkgs.ghostty-bin];
     };
 
-    darwinModules.default = {
-      homebrew.casks = ["ghostty"];
+    homeModules.alyGhostty = {
+      pkgs,
+      lib,
+      ...
+    }: {
+      programs.ghostty = {
+        enable = true;
+        package = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin pkgs.ghostty-bin;
+
+        settings =
+          {
+            theme = "Catppuccin Frappe";
+            notify-on-command-finish = "unfocused";
+            tab-inherit-working-directory = false;
+            window-inherit-working-directory = false;
+            font-family = "CaskaydiaCove Nerd Font";
+          }
+          // lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
+            gtk-titlebar-style = "tabs";
+            window-theme = "dark";
+            linux-cgroup = "always";
+          };
+      };
+    };
+
+    nixosModules.aly = {pkgs, ...}: {
+      users.users.aly.packages = [pkgs.ghostty];
     };
 
     systemModules.default = {pkgs, ...}: {
