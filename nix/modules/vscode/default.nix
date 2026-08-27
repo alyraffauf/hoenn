33cf04497d848f8b6f2b2045bd61f24edc2ae3ea
Aly Raffauf <aly@aly.codes>
2026-08-27 07:47:13 -0400

    vscode: add mutable settings

--- nix/modules/vscode/default.nix
+++ nix/modules/vscode/default.nix
@@ -1,5 +1,58 @@
 _: {
   flake.homeModules.vscode = {pkgs, ...}: {
-    home.packages = [pkgs.vscode];
+    programs.vscode = {
+      enable = true;
+      mutableExtensionsDir = true;
+
+      profiles.default = {
+        mutableUserSettings = true;
+        enableUpdateCheck = false;
+
+        extensions = with pkgs; [
+          vscode-extensions.catppuccin.catppuccin-vsc
+          vscode-extensions.catppuccin.catppuccin-vsc-icons
+          vscode-extensions.jnoortheen.nix-ide
+          vscode-extensions.mkhl.direnv
+          vscode-extensions.ms-vscode-remote.remote-ssh
+        ];
+
+        userSettings = {
+          "catppuccin.italicComments" = false;
+          "catppuccin.italicKeywords" = false;
+          "chat.agent.enabled" = true;
+          "chat.byokUtilityModelDefault" = "mainAgent";
+          "chat.disableAIFeatures" = false;
+          "chat.editor.fontSize" = 15;
+          "chat.fontSize" = 15;
+          "chat.viewSessions.orientation" = "stacked";
+          "editor.fontFamily" = "'CaskaydiaCove Nerd Font', Menlo, Monaco, 'Courier New', monospace";
+          "editor.fontSize" = 14;
+          "editor.formatOnPaste" = true;
+          "editor.formatOnSave" = true;
+          "editor.minimap.size" = "fill";
+          "editor.tabCompletion" = "on";
+          "editor.wordWrap" = "wordWrapColumn";
+          "files.autoSave" = "afterDelay";
+          "files.autoSaveDelay" = 500;
+          "files.autoSaveWorkspaceFilesOnly" = true;
+          "files.trimFinalNewlines" = true;
+          "files.trimTrailingWhitespace" = true;
+          "git.autofetch" = true;
+          "git.confirmSync" = false;
+          "git.enableSmartCommit" = true;
+          "git.openRepositoryInParentFolders" = "always";
+          "github.copilot.completions.chat.enabled" = true;
+          "github.copilot.nextEditSuggestions.enabled" = true;
+          "terminal.integrated.fontLigatures.enabled" = true;
+          "workbench.activityBar.compact" = true;
+          "workbench.browser.searchEngine" = "duckduckgo";
+          "workbench.colorTheme" = "Catppuccin Frappé";
+          "workbench.editor.editorActionsLocation" = "titleBar";
+          "workbench.iconTheme" = "catppuccin-frappe";
+          "catppuccin.accentColor" = "pink";
+          "catppuccin-icons.monochrome" = true;
+        };
+      };
+    };
   };
 }
