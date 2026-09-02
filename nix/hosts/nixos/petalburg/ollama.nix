d8119571795bf4e4098e696856ae463fbff00a5b
Aly Raffauf <aly@aly.codes>
2026-09-02 00:36:05 -0400

    petalburg: enable ollama with rocm

---
+++ nix/hosts/nixos/petalburg/ollama.nix
@@ -0,0 +1,9 @@
+_: {
+  flake.nixosModules.petalburg = {pkgs, ...}: {
+    services.ollama = {
+      enable = true;
+      package = pkgs.ollama-rocm;
+      rocmOverrideGfx = "10.3.0";
+    };
+  };
+}
